0d6a092422040b5bc0bbeb2807c2de03454cc106
shadow3 <shadow3aaa@qq.com>
2024-06-03 22:31:21 +0800

    style: 配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:vue/vue3-strongly-recommended"
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-    'vue/no-deprecated-slot-attribute': 'off'
   }
 }
