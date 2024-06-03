9ff720e76231449884a15034000b84d6391dc298
shadow3 <shadow3aaa@qq.com>
2024-06-03 22:31:52 +0800

    feat: project ui

--- src/ProjectView.vue
+++ src/ProjectView.vue
@@ -2,8 +2,11 @@
 import 'mdui/components/layout.js';
 import 'mdui/components/layout-item.js';
 import 'mdui/components/layout-main.js';
+import 'mdui/components/top-app-bar.js';
+import 'mdui/components/top-app-bar-title.js';
 import 'mdui/components/navigation-rail.js';
 import 'mdui/components/navigation-rail-item.js';
+import '@mdui/icons/file-open.js';
 
 </script>
 
@@ -12,5 +15,12 @@ import 'mdui/components/navigation-rail-item.js';
         <mdui-top-app-bar class="example-top-app-bar">
             <mdui-top-app-bar-title>Top App Bar</mdui-top-app-bar-title>
         </mdui-top-app-bar>
+        <mdui-navigation-rail value="Files">
+            <mdui-navigation-rail-item alue="Files">
+                Files
+                <span slot="icon"><mdui-icon-file-open></mdui-icon-file-open></span>
+            </mdui-navigation-rail-item>
+        </mdui-navigation-rail>
+        <mdui-layout-main class="example-layout-main" style="min-height: 300px">Main</mdui-layout-main>
     </mdui-layout>
 </template>
