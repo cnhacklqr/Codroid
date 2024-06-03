14be273e33ee89553e356e68fd2ef019a439f708
shadow3 <shadow3aaa@qq.com>
2024-06-03 22:18:02 +0800

    refactor:     fix: 错误的slot

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -20,23 +20,17 @@ function routeToProjectView() {
     <mdui-icon-android class="titleIcon"></mdui-icon-android>
     <h1 class="title">Codroid</h1>
 
-    <mdui-list class="menu">
+    <mdui-list>
       <mdui-list-item headline="Open Project" description="...and start coding" @click="routeToProjectView">
-        <template v-slot:icon>
-<span ><mdui-icon-add></mdui-icon-add></span>
-</template>
+        <span slot="icon"><mdui-icon-add></mdui-icon-add></span>
       </mdui-list-item>
 
-      <mdui-list-item headline="Settings" description="configure codroid">
-        <template v-slot:icon>
-<span ><mdui-icon-settings></mdui-icon-settings></span>
-</template>
+      <mdui-list-item headline="Settings" description="configure Codroid">
+        <span slot="icon"><mdui-icon-settings></mdui-icon-settings></span>
       </mdui-list-item>
 
       <mdui-list-item headline="About" description="author, licenses, etc.">
-        <template v-slot:icon>
-<span ><mdui-icon-info></mdui-icon-info></span>
-</template>
+        <span slot="icon"><mdui-icon-info></mdui-icon-info></span>
       </mdui-list-item>
 
     </mdui-list>
@@ -52,18 +46,10 @@ function routeToProjectView() {
 .titleIcon {
   margin: auto;
   font-size: 130px;
-  padding-top: 25px;
 }
 
 .title {
   margin: auto;
   font-size: 30px;
-  padding-top: 10px;
-}
-
-.menu {
-  margin: auto;
-  padding: 25px;
-  padding-top: 80px;
 }
 </style>
