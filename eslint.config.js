a9b17b70e98453afac421c7f4ca98adbb0005f24
shadow3aaa <shadow3aaa@qq.com>
2024-06-04 03:29:25 +0000

    style: 配置eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,26 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   ...pluginVue.configs["flat/strongly-recommended"],
   ...pluginVue.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
   {
     rules: {
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
-      'vue/no-deprecated-slot-attribute': 'off'
-    }
-  }
+      "vue/no-deprecated-slot-attribute": "off",
+    },
+  },
+  {
+    ignores: ["src/vite-env.d.ts"],
+  },
 ];
