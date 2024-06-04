14873c59ddc4f755ba607563723963ac90e46a3f
shadow3aaa <shadow3aaa@qq.com>
2024-06-04 14:56:30 +0000

    build: 优化打包

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,33 @@ export default defineConfig(async () => ({
       include: ["src/**/*.vue", "src/**/*.ts", "src/**/*.js"],
     }),
   ],
+  
+  optimizeDeps: {
+    include: [
+      "monaco-editor/esm/vs/language/json/json.worker",
+      "monaco-editor/esm/vs/language/css/css.worker",
+      "monaco-editor/esm/vs/language/html/html.worker",
+      "monaco-editor/esm/vs/language/typescript/ts.worker",
+      "monaco-editor/esm/vs/editor/editor.worker",
+    ],
+  },
+
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
