d0f6d0f7c60095d86135346291224396a42e711b
shadow3aaa <shadow3aaa@qq.com>
2024-06-05 12:58:43 +0000

    build: 优化vite打包

--- vite.config.ts
+++ vite.config.ts
@@ -27,14 +27,19 @@ export default defineConfig(async () => ({
     chunkSizeWarningLimit: 5000,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
+        manualChunks: {         
+          codemirror: [
+            // Split CodeMirror code.
+            'vue-codemirror6',
+            'codemirror',
+            '@codemirror/autocomplete',
+            '@codemirror/commands',
+            '@codemirror/language',
+            '@codemirror/lint',
+            '@codemirror/search',
+            '@codemirror/state',
+            '@codemirror/view',
+          ],
         },
       },
     },
