c47205cd2ea9fc48b222052b0e4f33423f61b861
shadow3 <shadow3aaa@qq.com>
2024-06-07 19:17:27 +0800

    fix: 修复提取rootfs错误逻辑

--- src-tauri/src/proot.rs
+++ src-tauri/src/proot.rs
@@ -1,5 +1,6 @@
 use std::fs::{self, File};
 
+use anyhow::Result;
 use zip::ZipArchive;
 
 use crate::{
@@ -7,23 +8,26 @@ use crate::{
     res::Resources,
 };
 
-pub fn setup_rootfs() {
+pub fn setup_rootfs() -> Result<()> {
     let linux_root = private_android_data().join("linux_root");
 
     if linux_root
         .read_dir()
         .ok()
         .and_then(|mut iter| iter.next())
-        .is_some()
+        .is_none()
     // Check whether the folder is empty or does not exist
     {
+        fs::create_dir(&linux_root)?;
         let rootfs_path = Resources::resources_dir()
             .join("rootfs")
             .join("archlinux-aarch64-rootfs.tar.xz");
-        let mut rootfs = File::open(rootfs_path).unwrap();
-        let mut rootfs = ZipArchive::new(rootfs).unwrap();
+        let rootfs = File::open(rootfs_path)?;
+        let mut rootfs = ZipArchive::new(rootfs)?;
 
         let cache_dir = private_android_cache();
-        rootfs.extract(&cache_dir).unwrap();
+        rootfs.extract(&cache_dir)?;
     }
+
+    Ok(())
 }
