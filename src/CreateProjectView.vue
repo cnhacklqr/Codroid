cf71f95913c9a90dbf511947fadd6767f0c4a1d8
shadow3 <shadow3aaa@qq.com>
2024-06-12 15:26:26 +0800

    refactor: 项目创建页面

---
+++ src/CreateProjectView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const step = ref(0);
+const stepPrefixed = computed(() => step.value + 1);
+
+const next = () => (step.value += 1);
+const prev = () => (step.value -= 1);
+</script>
+
+<template>
+  <v-stepper v-model="step" alt-labels class="view">
+    <v-stepper-header>
+      <v-stepper-item
+        title="Name your project"
+        value="1"
+        :complete="1 < stepPrefixed"
+      ></v-stepper-item>
+
+      <v-divider></v-divider>
+
+      <v-stepper-item
+        title="Select Template"
+        value="2"
+        :complete="2 < stepPrefixed"
+      ></v-stepper-item>
+    </v-stepper-header>
+
+    <v-stepper-window>
+      <v-stepper-window-item value="1"></v-stepper-window-item>
+    </v-stepper-window>
+
+    <v-stepper-actions
+      @click:next="next"
+      @click:prev="prev"
+    ></v-stepper-actions>
+  </v-stepper>
+</template>
+
+<style scoped>
+.view {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+}
+</style>
