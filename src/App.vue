49695bf0beef83e9e9444aa5d87843e6c5f4db66
shadow3 <shadow3aaa@qq.com>
2024-06-12 20:38:07 +0800

    feat: 添加全局app bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { provide, ref } from "vue";
+
+const appBarTitle = ref("");
+const appBarSubTitle = ref("");
+
+provide("appBarTitle", appBarTitle.value);
+provide("appBarSubTitle", appBarSubTitle.value);
+</script>
 
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade">
-      <component :is="Component" />
-    </transition>
-  </router-view>
+  <v-app class="rounded rounded-md">
+    <v-app-bar>
+      <template #prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+    </v-app-bar>
+
+    <v-main
+      ><router-view v-slot="{ Component }">
+        <transition name="fade">
+          <component :is="Component" />
+        </transition> </router-view
+    ></v-main>
+  </v-app>
 </template>
