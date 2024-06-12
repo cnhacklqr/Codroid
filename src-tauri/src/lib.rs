0240148feb918240af46a39a90e990dbef0d2ea3
shadow3 <shadow3aaa@qq.com>
2024-06-12 21:36:56 +0800

    feat: sidebar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -48,6 +48,7 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![init_resources])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <script setup lang="ts">
 import { provide, ref } from "vue";
+import { exit } from "@tauri-apps/plugin-process";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const appBarTitle = ref("");
-
 provide("appBarTitle", (newTitle: string) => (appBarTitle.value = newTitle));
+
+const showSidebar = ref(false);
+
+const exitApp = async () => {
+  await exit(0);
+};
+
+const routeToHomeView = () => {
+  router.push("/");
+};
 </script>
 
 <template>
   <v-app class="rounded rounded-md">
-    <v-app-bar>
-      <template #prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template>
+    <v-app-bar prominent>
+      <v-app-bar-nav-icon
+        @click.stop="showSidebar = !showSidebar"
+      ></v-app-bar-nav-icon>
 
       <v-app-bar-title>{{ appBarTitle }}</v-app-bar-title>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="showSidebar">
+      <v-list>
+        <v-list-item
+          title="Home"
+          prepend-icon="mdi-home"
+          rounded="xl"
+          @click="routeToHomeView"
+        >
+        </v-list-item>
+
+        <v-list-item
+          title="Exit"
+          prepend-icon="mdi-exit-to-app"
+          rounded="xl"
+          @click="exitApp"
+        >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main
       ><router-view v-slot="{ Component }">
