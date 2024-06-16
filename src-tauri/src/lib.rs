ff48a846145fe915925fad74a24c694faa95fbfe
shadow3 <shadow3aaa@qq.com>
2024-06-16 11:25:46 +0800

    refactor: 使用重构的PathResolver

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,49 +1,46 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![warn(clippy::nursery)]
 #![allow(clippy::missing_panics_doc)]
-mod android;
 mod path_resolver;
 mod payload;
 mod proot;
 mod res;
 
 use log::error;
+use path_resolver::PathResolver;
 use tauri::{AppHandle, Manager};
 use tauri_plugin_log::{Target, TargetKind};
 
 use payload::Payload;
 use proot::setup_rootfs;
-use res::Resources;
 
 #[tauri::command]
 async fn init_resources(app: AppHandle) {
-    app.emit("setup-process", Payload::new("Checking resources".into()))
+    let path_resolver = PathResolver::new(app.clone());
+
+    app.emit("setup-process", Payload::new("Setting Codroid Home".into()))
         .unwrap_or_else(|e| error!("{e:?}"));
-    Resources::auto_update()
-        .await
-        .unwrap_or_else(|e| error!("{e:?}"));
+    path_resolver.setup();
+
     app.emit(
         "setup-process",
         Payload::new("Checking proot rootfs".into()),
     )
     .unwrap_or_else(|e| error!("{e:?}"));
-    setup_rootfs().unwrap_or_else(|e| error!("{e:?}"));
+    setup_rootfs(&path_resolver).unwrap_or_else(|e| error!("{e:?}"));
+
     app.emit("setup-process", Payload::new("All done.".into()))
         .unwrap_or_else(|e| error!("{e:?}"));
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    path_resolver::init();
     tauri::Builder::default()
         .plugin(
             tauri_plugin_log::Builder::new()
                 .targets([
                     Target::new(TargetKind::Stdout),
-                    Target::new(TargetKind::Folder {
-                        path: path_resolver::cache_dir(),
-                        file_name: None,
-                    }),
+                    Target::new(TargetKind::Stderr),
                 ])
                 .build(),
         )
--- src-tauri/src/proot.rs
+++ src-tauri/src/proot.rs
@@ -1,34 +1,32 @@
-use std::fs::{self, File};
+use std::fs;
 
 use anyhow::Result;
 use tar::Archive;
 use xz2::read::XzDecoder;
 
-use crate::path_resolver;
+use crate::{path_resolver::PathResolver, res::Resources};
 
-pub fn setup_rootfs() -> Result<()> {
-    let linux_root = path_resolver::data_dir().join("linux_root");
+pub fn setup_rootfs(path_resolver: &PathResolver) -> Result<()> {
+    let proot_root = path_resolver.codroid_home().join("proot_root");
 
-    if linux_root
+    if proot_root
         .read_dir()
         .ok()
         .and_then(|mut iter| iter.next())
         .is_none()
     // Check whether the folder is empty or does not exist
     {
-        let _ = fs::create_dir(&linux_root);
-
-        let rootfs_path = path_resolver::resources_dir()
-            .join("rootfs")
-            .join("archlinux-aarch64-rootfs.tar.xz");
+        let _ = fs::create_dir(&proot_root);
 
         // start unpack xz
-        let rootfs_tar_xz = File::open(rootfs_path)?;
-        let rootfs_tar_unxz = XzDecoder::new(rootfs_tar_xz);
+        let rootfs_tar_xz = Resources::get("rootfs/archlinux-aarch64-rootfs.tar.xz")
+            .unwrap()
+            .data;
+        let rootfs_tar_unxz = XzDecoder::new(rootfs_tar_xz.as_ref());
 
         // start unpack tar
         let mut rootfs_untar = Archive::new(rootfs_tar_unxz);
-        rootfs_untar.unpack(linux_root)?;
+        rootfs_untar.unpack(proot_root)?;
     }
 
     Ok(())
