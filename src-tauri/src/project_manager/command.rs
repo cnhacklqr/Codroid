00793be1852e19f7f9753a41d1cff9d5ec52f567
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-06-26 13:50:29 +0800

    fix: 项目信息未持久化保存

--- src-tauri/src/project_manager/command.rs
+++ src-tauri/src/project_manager/command.rs
@@ -23,18 +23,20 @@ pub fn project_manager_create_project(
 ) {
     state
         .read()
-        .project_infos
-        .write()
-        .infos
-        .insert(project_info.name.clone(), project_info);
+        .update_project_infos(|project_infos| {
+            project_infos
+                .infos
+                .insert(project_info.name.clone(), project_info);
+        })
+        .unwrap_or_else(|e| error!("{e:?}"));
 }
 
 #[tauri::command]
 pub fn project_manager_remove_project(project_name: String, state: State<ProjectManagerWarpper>) {
     state
         .read()
-        .project_infos
-        .write()
-        .infos
-        .remove(&project_name);
+        .update_project_infos(|project_infos| {
+            project_infos.infos.remove(&project_name);
+        })
+        .unwrap_or_else(|e| error!("{e:?}"));
 }
