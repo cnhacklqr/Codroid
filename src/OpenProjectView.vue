1338cb7c7e7f2b444b8138ce613373184ddf110c
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-06-26 13:42:37 +0800

    feat: 打开项目页面

--- src/OpenProjectView.vue
+++ src/OpenProjectView.vue
@@ -1,2 +1,41 @@
-<script setup lang="ts"></script>
-<template><div></div></template>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref, Ref } from "vue";
+import { ProjectInfos } from "./type";
+import { listen, UnlistenFn } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/core";
+import ProjectCard from "./components/ProjectCard.vue";
+
+const projectInfos: Ref<ProjectInfos | null> = ref(null);
+let unlisten: Promise<UnlistenFn> | null = null;
+
+const updateProjectInfo = () => {
+  invoke("project_manager_project_infos").then(
+    (infos) => (projectInfos.value = infos as ProjectInfos),
+  );
+};
+
+onMounted(() => {
+  updateProjectInfo();
+
+  unlisten = listen<ProjectInfos>("project-list-update", () => {
+    updateProjectInfo();
+  });
+
+  invoke("project_manager_init_watcher");
+});
+
+onUnmounted(() => unlisten?.then((unlisten) => unlisten()));
+</script>
+
+<template>
+  <v-item-group selected-class="bg-primary">
+    <v-col md="4">
+      <ProjectCard
+        v-for="(project, index) in projectInfos?.infos"
+        :key="index"
+        :info="project"
+        class="mb-4"
+      />
+    </v-col>
+  </v-item-group>
+</template>
