c0e8e3511c4ea858a87fc747da608a78dc4a5348
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-06-27 22:53:04 +0800

    fix: 修复安卓创建部分文件夹链接错误

--- src-tauri/src/path_resolver.rs
+++ src-tauri/src/path_resolver.rs
@@ -1,8 +1,9 @@
 use std::{
-    env, fs,
+    env, fs, io,
     path::{Path, PathBuf},
 };
 
+use log::error;
 use symlink::symlink_dir;
 use tauri::AppHandle;
 use tauri_plugin_android_utils::AndroidUtilsExt;
@@ -21,29 +22,36 @@ impl PathResolver {
         let _ = fs::create_dir(self.cache_dir());
 
         let bin_dir = self.bin_dir();
-        if !bin_dir.exists() {
-            if cfg!(target_os = "android") {
-                let native_lib_dir = self.app.android_utils().native_lib_directory().unwrap();
-                let _ = fs::remove_file(&bin_dir); // 修复: 不是第一次安装时下一行会panic(因为native_lib_dir安装时会变动?未求证)
-                symlink_dir(native_lib_dir, bin_dir).unwrap(); // api28后只有nativelib文件夹可以有可执行权限
-            } else {
-                let _ = fs::create_dir(bin_dir);
-            }
+
+        if cfg!(target_os = "android") {
+            let native_lib_dir = self.app.android_utils().native_lib_directory().unwrap();
+            android_recreate_symlink_dir(native_lib_dir, bin_dir)
+                .unwrap_or_else(|e| error!("{e:?}")); // api28后只有nativelib文件夹可以有可执行权限
+        } else if !bin_dir.exists() {
+            fs::create_dir(bin_dir).unwrap_or_else(|e| error!("{e:?}"));
         }
+
+        let project_dir = self.project_dir();
 
         #[cfg(target_os = "android")]
         {
-            let project_dir = self.project_dir();
+            let proot_home = self.proot_home_dir();
+            let proot_project_dir = proot_home.join("projects");
+
+            if !proot_project_dir.exists() {
+                fs::create_dir(&proot_project_dir).unwrap_or_else(|e| error!("{e:?}"));
+            }
+
+            android_recreate_symlink_dir(proot_project_dir, project_dir)
+                .unwrap_or_else(|e| error!("{e:?}"));
+        }
+
+        #[cfg(not(target_os = "android"))]
+        {
             if !project_dir.exists() {
-                let _ = fs::remove_file(&project_dir);
-                let proot_home = self.proot_home_dir();
-                let proot_project_dir = proot_home.join("projects");
-                let _ = fs::create_dir(&proot_project_dir);
-                symlink_dir(proot_project_dir, project_dir).unwrap();
+                fs::create_dir(self.project_dir()).unwrap_or_else(|e| error!("{e:?}"));
             }
         }
-        #[cfg(not(target_os = "android"))]
-        let _ = fs::create_dir(self.project_dir());
     }
 
     pub fn codroid_home(&self) -> PathBuf {
@@ -92,11 +100,22 @@ impl PathResolver {
 
     #[cfg(target_os = "android")]
     pub fn proot_root_dir(&self) -> PathBuf {
-        self.codroid_home().join("proot_root")
+        self.codroid_home()
+            .join("proot_root")
+            .join("archlinux-aarch64")
     }
 
     #[cfg(target_os = "android")]
     pub fn proot_home_dir(&self) -> PathBuf {
         self.proot_root_dir().join("root")
     }
+}
+
+fn android_recreate_symlink_dir<P, Q>(src: P, dst: Q) -> io::Result<()>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    let _ = fs::remove_file(dst.as_ref());
+    symlink_dir(src, dst)
 }
