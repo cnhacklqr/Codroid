9dde1fd2cf319e71a1598fce5516747f237c6cc6
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-07-02 21:05:34 +0800

    refactor: 调整codemirror

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import CodeMirror from "vue-codemirror6";
+import { tomorrow } from "./codemirrorTheme";
 
 const value = ref("");
+
+const extensions = [tomorrow];
 </script>
 
 <template>
-  <code-mirror v-model="value" basic />
+  <code-mirror v-model="value" basic :extensions="extensions" />
 </template>
