2b04b72764c36ab2ef5fecfe5449cd1665a97ff2
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-07-05 11:36:19 +0800

    fix: 创建项目页面没有切换动画

--- src/CreateProjectView.vue
+++ src/CreateProjectView.vue
@@ -64,121 +64,123 @@ const backToHome = () => router.replace("/");
 </script>
 
 <template>
-  <var-space align="center" justify="center">
-    <var-steps :active="step" class="stepper">
-      <var-step>Select Project Template</var-step>
-      <var-step>Configure Project</var-step>
-      <var-step>Confirm Project Information</var-step>
-    </var-steps>
-  </var-space>
+  <div>
+    <var-space align="center" justify="center">
+      <var-steps :active="step" class="stepper">
+        <var-step>Select Project Template</var-step>
+        <var-step>Configure Project</var-step>
+        <var-step>Confirm Project Information</var-step>
+      </var-steps>
+    </var-space>
 
-  <var-space align="center" justify="center">
-    <var-card
-      v-if="stepPrefixed === 1"
-      title="Create New Project"
-      subtitle="Select Project Template"
-      class="stepper-content"
-    >
-      <template #description>
-        <var-space>
-          <p class="description">
-            1. Understand project type: Determine the type of project before
-            selecting a template.<br />
-            2. Plan project: Consider long-term plan and goals when choosing a
-            template.<br />
-            3. Compare options: Choose from multiple templates with strengths
-            and weaknesses in mind.<br />
-          </p>
-        </var-space>
-
-        <var-space>
-          <var-select v-model="projectTemplate" class="input">
-            <var-option
-              v-for="(labal, index) in templateList"
-              :key="index"
-              placeholder="empty"
-              :label="labal"
-            />
-          </var-select>
-        </var-space>
-      </template>
-
-      <template #extra>
-        <var-space>
-          <var-button type="primary" @click="prev">Prev</var-button>
-          <var-button
-            type="primary"
-            :disabled="nextButtonDisabled"
-            @click="next"
-            >Next</var-button
-          >
-        </var-space>
-      </template>
-    </var-card>
-
-    <var-card
-      v-if="stepPrefixed === 2"
-      title="Create New Project"
-      subtitle="Configure Project"
-      class="stepper-content"
-    >
-      <template #description>
-        <var-space>
-          <p class="description">
-            1. Use clear and concise language.<br />
-            2. Focus on the most important information.<br />
-            3. Avoid unnecessary words or phrases.<br />
-            4. Keep it simple and easy to understand.<br />
-          </p>
-        </var-space>
-
-        <var-space>
-          <var-input
-            v-model="projectName"
-            placeholder="Project Name"
-            class="input"
-          />
-        </var-space>
-      </template>
-
-      <template #extra>
-        <var-space>
-          <var-button type="primary" @click="prev">Prev</var-button>
-          <var-button
-            type="primary"
-            :disabled="nextButtonDisabled"
-            @click="confirmCreation"
-            >Create Project</var-button
-          >
-        </var-space>
-      </template>
-    </var-card>
-
-    <var-popup
-      v-model:show="success"
-      :default-style="false"
-      :close-on-click-overlay="false"
-      :close-on-key-escape="false"
-    >
-      <var-result
-        class="result"
-        title="Successed"
-        description="Your Project was created successfully."
+    <var-space align="center" justify="center">
+      <var-card
+        v-if="stepPrefixed === 1"
+        title="Create New Project"
+        subtitle="Select Project Template"
+        class="stepper-content"
       >
-        <template #footer>
-          <var-space direction="row" align="center">
-            <var-button type="success" @click="openProject">
-              Open Project
-            </var-button>
+        <template #description>
+          <var-space>
+            <p class="description">
+              1. Understand project type: Determine the type of project before
+              selecting a template.<br />
+              2. Plan project: Consider long-term plan and goals when choosing a
+              template.<br />
+              3. Compare options: Choose from multiple templates with strengths
+              and weaknesses in mind.<br />
+            </p>
+          </var-space>
 
-            <var-button type="success" @click="backToHome">
-              Back to Home Page
-            </var-button>
+          <var-space>
+            <var-select v-model="projectTemplate" class="input">
+              <var-option
+                v-for="(labal, index) in templateList"
+                :key="index"
+                placeholder="empty"
+                :label="labal"
+              />
+            </var-select>
           </var-space>
         </template>
-      </var-result>
-    </var-popup>
-  </var-space>
+
+        <template #extra>
+          <var-space>
+            <var-button type="primary" @click="prev">Prev</var-button>
+            <var-button
+              type="primary"
+              :disabled="nextButtonDisabled"
+              @click="next"
+              >Next</var-button
+            >
+          </var-space>
+        </template>
+      </var-card>
+
+      <var-card
+        v-if="stepPrefixed === 2"
+        title="Create New Project"
+        subtitle="Configure Project"
+        class="stepper-content"
+      >
+        <template #description>
+          <var-space>
+            <p class="description">
+              1. Use clear and concise language.<br />
+              2. Focus on the most important information.<br />
+              3. Avoid unnecessary words or phrases.<br />
+              4. Keep it simple and easy to understand.<br />
+            </p>
+          </var-space>
+
+          <var-space>
+            <var-input
+              v-model="projectName"
+              placeholder="Project Name"
+              class="input"
+            />
+          </var-space>
+        </template>
+
+        <template #extra>
+          <var-space>
+            <var-button type="primary" @click="prev">Prev</var-button>
+            <var-button
+              type="primary"
+              :disabled="nextButtonDisabled"
+              @click="confirmCreation"
+              >Create Project</var-button
+            >
+          </var-space>
+        </template>
+      </var-card>
+
+      <var-popup
+        v-model:show="success"
+        :default-style="false"
+        :close-on-click-overlay="false"
+        :close-on-key-escape="false"
+      >
+        <var-result
+          class="result"
+          title="Successed"
+          description="Your Project was created successfully."
+        >
+          <template #footer>
+            <var-space direction="row" align="center">
+              <var-button type="success" @click="openProject">
+                Open Project
+              </var-button>
+
+              <var-button type="success" @click="backToHome">
+                Back to Home Page
+              </var-button>
+            </var-space>
+          </template>
+        </var-result>
+      </var-popup>
+    </var-space>
+  </div>
 </template>
 
 <style scoped>
